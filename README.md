507bb2543db79a2e625d88cf81101ad0cf860fe1
martin <z74ma@uwaterloo.ca>
2020-08-22 19:03:58 +0800

    DDQN + PER

--- README.md
+++ README.md
@@ -1,1 +1,112 @@
-# tetris-ai
+# [PYTORCH] Deep Q-learning for playing Tetris
+
+## Introduction
+
+Here is my python source code for training an agent to play Tetris. It uses a double DQN (Deep Q-Network) with PER (Prioritized Experience Replay).
+
+<p align="center">
+  <img src="demo/tetris-ddqn-per.gif" width=600><br/>
+  <i>Tetris demo</i>
+</p>
+
+The full demo can be found at [here](https://www.youtube.com/watch?v=fKL5muTWCb0).
+
+## How it works
+### States & Actions
+The input of the model is a list of states, each of them is reached by taking a particular action from the previous state. The state is characterized by 4 variables, which are [heuristically](https://codemyroad.wordpress.com/2013/04/14/tetris-ai-the-near-perfect-player/) believed to be enough for Tetris. The 4 variables are:
+- Number of lines cleared: captures immediate rewards
+- Number of holes created: captures local disadvantage
+- Bumpiness: captures global disadvantage
+- Height: captures global disadvantage
+
+Instead of characterize the actions by *move left*, *move right* and *rotate* at each time step, we are taking a different approach and characterize the actions by:
+- Number of rotations:
+	- For line, square and Z-shape tetrominos, #rotation = {0, 1}
+	- For L-shape and T-shape tetrominos, #rotation= {0, 1, 2, 3}
+- Location to drop:
+	- This is an integer between 0 and the width of the board, given that the location is valid (all parts of the tetromino is in the board).
+
+To put these together, the input of the model is of shape (N, C):
+- N is the number of possible actions at that current state. N = # of possible rotations * # of possible places to put the tetrimino
+- C is the number of attribute we track to make a decision. C = 4 in this case.
+
+### Model
+The bare-bone deep Q-Learning model is adapted from [here](https://github.com/uvipen/Tetris-deep-Q-learning-pytorch), the objective is to minimize the MSE of r<sub>j</sub> + \gamma max{q(s<sub>j+1</sub>)} - q(s<sub>j</sub>).
+
+<p align="center">
+  <img src="images/dqn_objective.jpg"/>
+</p>
+
+y<sub>t</sub><sup>DQN</sup> for one-step estimator learning target is basically the same as TD(0):
+
+<p align="center">
+  <img src="images/dqn_y.jpg"/>
+</p>
+
+Since this is a value-based approach, the optimal policy is found by picking the action that could optimize the value function given a particular state.
+
+Furthermore, I added some functionalities to the training algorithm to allow faster and more efficient training. Taking the ideas from the [AlphaGo paper](https://www.nature.com/articles/nature16961) and the [Prioritized Experience Replay paper](https://arxiv.org/abs/1511.05952), I implmented double Q-learning and prioritized experience replay to achieve better sample efficiency.
+
+### Double Deep Q-Learning
+The benefit of double deep Q-learning is twofold.
+1. It reduces [maximization bias](https://towardsdatascience.com/double-deep-q-networks-905dd8325412).
+2. It helps stabilize training by avoiding "chasing a moving target".
+
+Maximization bias is introduced in Q-learning when we take the maximum of various Q-value approximations. To resolve this, we need to separate the evaluation and action selection into two networks. This has been [shown](https://papers.nips.cc/paper/3964-double-q-learning) to significanty speed up training time by eliminating suboptimal actions more quickly than normal Q-learning.
+
+Furthermore, we can update the evaluation network (target network) periodically instead of updating at every step of training. By doing so, we are not using a constantly shifting set of values to adjust our network; therefore, preventing from falling into a feedback loop between the target and estimated Q-value. By setting the the target network constant for a certain number of steps, the behaviour of the RL agent is also stabilized. The frequency at which the target updates is an important parameter, setting the frequency too high would deteriorate the stabilization effects, setting it too low inhibits learning.
+
+y<sub>t</sub><sup>DoubleDQN</sup> for one-step estimator learning target is shown below. W represents the weights for the Q-value estimation networks and W' is the weights for the target network.
+
+<p align="center">
+  <img src="images/ddqn_y.jpg"
+  height="38"
+  >
+</p>
+
+### Prioritized Experience Replay
+Data collected by the RL agent is usally not iid, unlike supervised learning. The reward you will get at the next timestep is highly related to the reward you have at the current timestep. To improve data efficiency and remove sample correlations, minibatch is sampled from a pool of stored experiences, called replay buffer.
+
+In the original implementation, the experience replay is random. In this [paper](https://arxiv.org/abs/1511.05952), Tom Schaul states that some experiences may be more important than others. They could be game changers but they don't occur often; whereas some less important experience may occur frequently. Thus, if we uniformly sample the replay buffer, we might not be able to sample the game changers many times before it's pushed out of the replay buffer.
+
+To tackle this problem, we assign a priority value based on TD error. `(state, action, reward, next_state)` tuple with high error should be selected more often during the training process. The priority value for stochastic prioritization is defined as below:
+
+<p align="center">
+  <img src="images/priority.jpg"
+  height="50"
+  >
+</p>
+
+where:
+- P(i) is the priority value
+- p<sub>i</sub> is the TD error
+- a is a hyperparameter used to introduce some randomness in the experience replay process
+  - If a is 0, pure randomness (like the original model)
+  - If a is 1, we always selects the experience with the highest priorities
+- The summation in the denominator acts as a normalization by all priority values in the replay buffer.
+
+Implementation using a normal deque or an array (in random experience replay) wouldn't be computationally efficient in this case, as both insertion and sampling are both O(n) per sample in the minibatch. Rather, using a data structure like priority queue would allow O(logn) for both insertion and sampling. In my implementation, a [SumTree](https://adventuresinmachinelearning.com/sumtree-introduction-python/) is used.
+
+## How to use my code
+
+With my code, you can:
+* **Train your model from scratch** by running **python train.py**
+* **Test your trained model** by running **python test.py**
+
+## Trained models
+
+You could find my trained model at **trained_models/tetris**
+ 
+## Requirements
+
+* **python 3.6**
+* **PIL**
+* **cv2**
+* **pytorch** 
+* **numpy**
+* **matplotlib**
+
+## Acknowledgement
+I learned and explored many of the above concepts from [CS234](https://web.stanford.edu/class/cs234/index.html) at Stanford, which is an awesome course to get a comprehensive Intro to RL.
+
+Thanks [unipen](https://github.com/uvipen) for the baseline model, which I based off to develop Double Q-Learning and Prioritized Experience Replay.
---
+++ train.py
@@ -0,0 +1,188 @@
+"""
+@author: Viet Nguyen <nhviet1009@gmail.com>
+"""
+import argparse
+import os
+import shutil
+from random import random, randint, sample
+
+import numpy as np
+import torch
+import torch.nn as nn
+from tensorboardX import SummaryWriter
+
+from src.deep_q_network import DeepQNetwork
+from src.tetris import Tetris
+from src.PER import Memory
+from collections import deque
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        """Implementation of Deep Q Network to play Tetris""")
+    parser.add_argument("--width", type=int, default=10, help="The common width for all images")
+    parser.add_argument("--height", type=int, default=20, help="The common height for all images")
+    parser.add_argument("--block_size", type=int, default=30, help="Size of a block")
+    parser.add_argument("--batch_size", type=int, default=512, help="The number of images per batch")
+    parser.add_argument("--lr", type=float, default=1e-3)
+    parser.add_argument("--gamma", type=float, default=0.99)
+    parser.add_argument("--initial_epsilon", type=float, default=1)
+    parser.add_argument("--final_epsilon", type=float, default=1e-3)
+    parser.add_argument("--num_decay_epochs", type=float, default=2000)
+    parser.add_argument("--num_epochs", type=int, default=3000)
+    parser.add_argument("--save_interval", type=int, default=1000)
+    parser.add_argument("--replay_memory_size", type=int, default=30000,
+                        help="Number of epoches between testing phases")
+    parser.add_argument("--log_path", type=str, default="tensorboard")
+    parser.add_argument("--saved_path", type=str, default="trained_models")
+    parser.add_argument("--target_update_freq", type=int, default=10)
+    parser.add_argument("--PER", type=bool, default=True, help="Use to priority experience replay")
+    parser.add_argument("--learning_starts", type=int, default=100, help="After how many epochs to start learning")
+
+    args = parser.parse_args()
+    return args
+
+
+def train(opt):
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(123)
+    else:
+        torch.manual_seed(123)
+    if os.path.isdir(opt.log_path):
+        shutil.rmtree(opt.log_path)
+    os.makedirs(opt.log_path)
+    writer = SummaryWriter(opt.log_path)
+    env = Tetris(width=opt.width, height=opt.height, block_size=opt.block_size)
+    model = DeepQNetwork()
+    model_target = DeepQNetwork()
+    optimizer = torch.optim.Adam(model.parameters(), lr=opt.lr)
+    criterion = nn.MSELoss()
+
+    state = env.reset()
+    if torch.cuda.is_available():
+        model.cuda()
+        model_target.cuda()
+        state = state.cuda()
+
+    if opt.PER:
+        replay_memory = Memory(capacity=opt.replay_memory_size)
+    else:
+        replay_memory = deque(maxlen=opt.replay_memory_size)
+
+    epoch = 0
+    warmup_epoch = 0
+    while epoch < opt.num_epochs:
+        next_steps = env.get_next_states()
+        # Exploration or exploitation
+        epsilon = opt.final_epsilon + (max(opt.num_decay_epochs - epoch, 0) * (
+                opt.initial_epsilon - opt.final_epsilon) / opt.num_decay_epochs)
+        u = random()
+        random_action = u <= epsilon
+        next_actions, next_states = zip(*next_steps.items())
+        next_states = torch.stack(next_states)
+        if torch.cuda.is_available():
+            next_states = next_states.cuda()
+        model.eval()
+        with torch.no_grad():
+            predictions = model(next_states)[:, 0]
+        model.train()
+        if random_action:
+            index = randint(0, len(next_steps) - 1)
+        else:
+            index = torch.argmax(predictions).item()
+
+        next_state = next_states[index, :]
+        action = next_actions[index]
+
+        reward, done = env.step(action, render=True)
+
+        if torch.cuda.is_available():
+            next_state = next_state.cuda()
+
+        if opt.PER:
+            experience = state, action, reward, next_state, done
+            replay_memory.store(experience)
+        else:
+            replay_memory.append([state, reward, next_state, done])
+
+        if done:
+            final_score = env.score
+            final_tetrominoes = env.tetrominoes
+            final_cleared_lines = env.cleared_lines
+            state = env.reset()
+            if torch.cuda.is_available():
+                state = state.cuda()
+        else:
+            state = next_state
+            continue
+        warmup_epoch += 1
+        if warmup_epoch < opt.learning_starts:
+            continue
+        epoch += 1
+
+        if opt.PER:
+            tree_idx, batch = replay_memory.sample(opt.batch_size)
+        else:
+            batch = sample(replay_memory, min(len(replay_memory), opt.batch_size))
+
+        state_batch, _, reward_batch, next_state_batch, done_batch = zip(*batch)
+        state_batch = torch.stack(tuple(state for state in state_batch))
+        reward_batch = torch.from_numpy(np.array(reward_batch, dtype=np.float32)[:, None])
+        next_state_batch = torch.stack(tuple(state for state in next_state_batch))
+
+        if torch.cuda.is_available():
+            state_batch = state_batch.cuda()
+            reward_batch = reward_batch.cuda()
+            next_state_batch = next_state_batch.cuda()
+
+        q_values = model(state_batch)
+        model_target.eval()
+        with torch.no_grad():
+            next_prediction_batch = model_target(next_state_batch)
+        model_target.train()
+
+        y_batch = torch.cat(
+            tuple(reward if done else reward + opt.gamma * prediction for reward, done, prediction in
+                  zip(reward_batch, done_batch, next_prediction_batch)))[:, None]
+
+        optimizer.zero_grad()
+        loss = criterion(q_values, y_batch)
+        loss.backward()
+        optimizer.step()
+
+        model.eval()
+        model_target.eval()
+        if opt.PER:
+            with torch.no_grad():
+                if torch.cuda.is_available():
+                    replay_memory.batch_update(tree_idx, np.abs(q_values.detach().cpu().numpy()-y_batch.cpu().numpy()))
+                else:
+                    replay_memory.batch_update(tree_idx, np.abs(q_values.detach().numpy()-y_batch.numpy()))
+        
+        # Update target model <- model
+        if epoch % opt.target_update_freq == 0:
+            with torch.no_grad():
+                model_target.load_state_dict(model.state_dict())
+        model_target.train()
+        model.eval()
+
+        print("Epoch: {}/{}, Action: {}, Score: {}, Tetrominoes {}, Cleared lines: {}".format(
+            epoch,
+            opt.num_epochs,
+            action,
+            final_score,
+            final_tetrominoes,
+            final_cleared_lines))
+        writer.add_scalar('Train/Score', final_score, epoch - 1)
+        writer.add_scalar('Train/Tetrominoes', final_tetrominoes, epoch - 1)
+        writer.add_scalar('Train/Cleared lines', final_cleared_lines, epoch - 1)
+
+        if (epoch > 0 and epoch % opt.save_interval) == 0 or final_score >= 10000.0:
+            torch.save(model, "{}/tetris_{}".format(opt.saved_path, epoch))
+
+    torch.save(model, "{}/tetris".format(opt.saved_path))
+
+
+if __name__ == "__main__":
+    opt = get_args()
+    train(opt)
